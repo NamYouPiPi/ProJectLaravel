5a257d44ffb09f571469cac45806d6eda5db1e9f
namyou11122003 <namyou854@gmail.com>
2025-07-30 12:30:07 +0700

    add dashboad

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,19 @@
+       <!-- Links -->
+                <ul class="sidenav-inner py-1">
+                    <!-- UI elements -->
+                    <li class="sidenav-item">
+                        <a href="javascript:" class="sidenav-link sidenav-toggle">
+                            <i class="sidenav-icon feather icon-box"></i>
+                            <div>UI components</div>
+                        </a>
+                        <ul class="sidenav-menu">
+                            <li class="sidenav-item">
+                                <a href="bui_alert.html" class="sidenav-link">
+                                    <div>Alerts</div>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+               
+                
+                </ul>
